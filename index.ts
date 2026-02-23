7ad04427589cffba7968a574be6b82dfdf1ce7b8
HarmishTervadiya <harmishtervadiya@gmail.com>
2026-02-23 19:20:37 +0530

    feat: Implement create spl token and transfer token

---
+++ index.ts
@@ -0,0 +1,37 @@
+const { getOrCreateAssociatedTokenAccount, transfer } = require("@solana/spl-token");
+const { createMint } = require('@solana/spl-token');
+const { Keypair, Connection, clusterApiUrl,  TOKEN_PROGRAM_ID, PublicKey } = require('@solana/web3.js');
+
+
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed")
+
+const payer=Keypair.fromSecretKey(Uint8Array.from([168,25,31,223,217,164,188,212,136,174,44,1,81,125,200,137,3,105,13,178,37,64,233,201,131,130,220,32,171,125,104,195,73,65,46,72,72,129,192,198,254,126,163,187,165,5,54,181,41,42,139,14,236,24,130,174,108,39,51,67,145,112,249,15])) 
+const mintAuthority = payer
+
+async function createMintForToken(payer: typeof Keypair, mintAuthority: typeof Keypair){
+    const mint = await createMint(connection, payer, mintAuthority.publicKey, null, 8, undefined, undefined,TOKEN_PROGRAM_ID)
+    console.log(`Mint create at: ${mint.toBase58()}`)
+    return mint
+}
+
+const recipient = new PublicKey("CL9gAAiFQJ5WZZDWN82wGegVCi2WzQHpeHLebt5gGNss")
+const mint = new PublicKey("DKbCyo85JMqJkysQzB5Ck4w7mr8nsfFG3tdHEcjTtFMX")
+
+
+
+async function transferToken(payer: typeof Keypair){
+const sourceATA = await getOrCreateAssociatedTokenAccount(connection, payer, mint, payer.publicKey)
+const recipientATA = await getOrCreateAssociatedTokenAccount(connection, payer, mint, recipient)
+
+const tx= await transfer(connection, payer, sourceATA.address, recipientATA.address, payer.publicKey, 50)
+    
+console.log(`tx created at signature: `, tx);
+return tx;
+}
+
+async function main(){
+    // const mint = await createMintForToken(payer, mintAuthority)
+    const transferToATA = transferToken(payer)
+}
+
+main()
